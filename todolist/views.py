34e728a46fc3a637a8ba83d6ec613ab6f0adc253
Nama Kamu <emailkamu@example.com>
2025-05-26 07:12:23 +0700

    upload project

--- todolist/views.py
+++ todolist/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
+from django.shortcuts import get_object_or_404
 
 def task_list(request):
     tasks = Task.objects.all()
 
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
+    
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
--- todolist_project/urls.py
+++ todolist_project/urls.py
@@ -21,5 +21,5 @@ from todolist import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.task_list, name='task_list'),
-
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),  # ⬅️ Tambahan ini
 ]
