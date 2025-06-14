3074686fde85aae62be3258934fd2cbb79720a75
Nama Kamu <emailkamu@example.com>
2025-06-14 11:21:50 +0700

    upload project

--- todolist/views.py
+++ todolist/views.py
@@ -15,7 +15,7 @@ def task_list(request):
 
 def complete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    task.completed = True  # ← pakai 'completed' (bukan 'is_completed') kalau model kamu namanya gitu
+    task.completed = True
     task.save()
     return redirect('task_list')
 
@@ -23,3 +23,24 @@ def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect('task_list')
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'todolist/edit_task.html', {'form': form})
+
+def task_filter(request, status):
+    if status == 'selesai':
+        tasks = Task.objects.filter(completed=True)
+    elif status == 'belum':
+        tasks = Task.objects.filter(completed=False)
+    else:
+        tasks = Task.objects.all()
+    form = TaskForm()
+    return render(request, 'todolist/task_list.html', {'tasks': tasks, 'form': form})
--- todolist_project/urls.py
+++ todolist_project/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
-
+    path('edit/<int:pk>/', views.edit_task, name='edit_task'),
+  
 ]
